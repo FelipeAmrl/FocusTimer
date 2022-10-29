65fda1cedcb692e07be6f53f12360b6d5b5ab17b
FelipeAmrl <felipeamaral.bio@gmail.com>
2022-10-29 17:31:25 -0300

    add timer events and functions

---
+++ js/elements.js
@@ -0,0 +1,17 @@
+const minutesDisplay = document.querySelector(".minutes");
+const secondsDisplay = document.querySelector(".seconds");
+const playButton = document.querySelector(".play");
+const pauseButton = document.querySelector(".pause");
+const stopButton = document.querySelector(".stop");
+const increaseButton = document.querySelector(".increase");
+const decreaseButton = document.querySelector(".decrease");
+
+export {
+    minutesDisplay,
+    secondsDisplay,
+    playButton,
+    pauseButton,
+    stopButton,
+    increaseButton,
+    decreaseButton
+}
